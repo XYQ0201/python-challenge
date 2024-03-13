1983626fa9e445429e8efbee1d9107af78e2acba
XYQ0201 <159677165+XYQ0201@users.noreply.github.com>
2024-03-13 19:07:46 +1100

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,6 @@
 import os 
 import csv
-csvpath=os.path.join('Pypoll/Resources','election_data.csv')
+csvpath=os.path.join('Resources','election_data.csv')
 with open(csvpath) as csvfile:
     csvreader =csv.reader(csvfile)
     csvheader=next(csvreader)
